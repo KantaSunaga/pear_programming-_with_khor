47ee3ddacd6ec5825594ee0ea30e527983a02a57
KantaSunaga <sunagakannta6@gmail.com>
2017-08-03 06:38:57 +0900

    バグア郵政

--- app/models/customer_user.rb
+++ app/models/customer_user.rb
@@ -1,8 +1,11 @@
 class CustomerUser < ApplicationRecord
 require 'csv'
   def self.import(file)
-    CSV.foreach(csv_file.path, headers: false) do |row|
-     CustomerUser.create! row.to_hash
+    CSV.foreach(file.path, headers: false) do |row|
+     CustomerUser.create({first_name: row[0],
+                          last_name: row[1],
+                          email: row[2],
+                          phone: row[3]})
     end
   end
 end
